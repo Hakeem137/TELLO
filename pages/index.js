718297f838fc53f5f7c3cdcb44670acaae4562ae
Ahmed Hakeem <ahmedra3d0100@gmail.com>
2025-09-13 00:13:32 +0300

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{maxWidth:800, margin:'40px auto', padding:20, textAlign:'center'}}>
-      <h1>Welcome to TELLO</h1>
-      <p>To test: create a user doc in Firestore with a username, then open /username</p>
-    </div>
-  );
-}
