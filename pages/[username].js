6a560aab01793eff463633800d6758806eda436f
Ahmed Hakeem <ahmedra3d0100@gmail.com>
2025-09-12 20:50:37 +0300

    Update [username].js

--- pages/[username].js
+++ pages/[username].js
@@ -54,3 +54,18 @@ export async function getServerSideProps({ params }) {
   const doc = users.docs[0].data();
   return { props: { user: { username: doc.username, name: doc.name || null, avatar_url: doc.avatar_url || null }, siteKey: process.env.RECAPTCHA_SITEKEY } };
 }
+
+import admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+  });
+}
+
+export default admin;
